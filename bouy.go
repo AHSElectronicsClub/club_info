c89bf96f9fb347ea77657c0ec62e7fe7b579f1ff
Sean Embley <seanmembley@proton.me>
2026-01-07 16:52:56 -0500

    feat: state enum

--- bouy.go
+++ bouy.go
@@ -1,26 +1,48 @@
 package main
 
 import (
+	"errors"
 	"time"
 
 	"github.com/google/uuid"
 )
 
+type BouyState int8
+
+const (
+	StateIdle BouyState = iota
+	StateActive
+	StateCollecting
+	StateDisabled
+)
+
+var StateMap = map[BouyState]string{
+	StateIdle:       "idle",
+	StateActive:     "active",
+	StateCollecting: "collecting",
+	StateDisabled:   "disabled",
+}
+
 type Bouy struct {
 	ID       uuid.UUID
 	Name     string
 	Battery  int16
+	State    BouyState
 	Scanners []Scanner
 }
 
 type Scanner struct {
 	ID      uuid.UUID
 	Battery int16
-	Data    []Measurement
+	Data    []Sample
 }
 
-type Measurement struct {
+type Sample struct {
 	Time          time.Time `json:"time"`
 	OxygenContent float64   `json:"oxygen_content"`
 	// All of the other data points will go here
+}
+
+func (b *Bouy) GetState() string {
+	return StateMap[b.State]
 }
